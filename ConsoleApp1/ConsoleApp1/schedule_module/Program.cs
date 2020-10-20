a7d13755a313a8c22b3dfc59d29dcb459e7cbd26
Adrian <Adrian@DESKTOP-JE8H084>
2020-10-20 13:05:53 +0300

    Separed schedule class from main class

---
+++ ConsoleApp1/ConsoleApp1/schedule_module/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Program
+{   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Contact contact1 = new Contact("John Doe", "email", "as soon as available");
+            Contact contact2 = new Contact("Jane Doe", "fax", "after 8 am");
+            Contact contact3 = new Contact("Collect", "email", "between 8 and 8:30 am");
+            List<Contact> contacts = new List<Contact>();
+            contacts.Add(contact1);
+            contacts.Add(contact2);
+            contacts.Add(contact3);
+            Client client1 = new Client("John Doe Medical Office", contacts);
+            Schedule schedule = new Schedule();
+            schedule.sendRaports(client1);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/schedule_module/Schedule.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Schedule
+    {
+        public void sendRaports(Client client)
+        {
+            Console.WriteLine("For " + client.name + ':');
+            Console.WriteLine();
+            foreach (Contact contact in client.contacts)
+            {
+                if (contact.deliveryHours.Equals("as soon as available"))
+                {
+                    sendRaport(contact);
+                }
+                else
+                {
+                    string[] words = contact.deliveryHours.Split(' ');
+                    TimeSpan time = DateTime.Now.TimeOfDay;
+                    if (words[0].Equals("after"))
+                    {
+                        //negleted minutes in hour verification 
+                        if (words[1].CompareTo(time.Hours.ToString()) < 0)
+                        {
+                            sendRaport(contact);
+                        }
+                        else
+                        {
+                            scheduleSendRaport(contact);
+                            // an email/fax/hl7 should be scheduled to be sent to the contact after the hour he set
+                        }
+                    }
+                    if (words[0].Equals("between"))
+                    {
+                        //negleted minutes
+                        if (words[1].CompareTo(time.Hours.ToString()) > 0 && words[3].CompareTo(time.Hours.ToString()) < 0)
+                        {
+                            sendRaport(contact);
+                        }
+                        else
+                        {
+                            scheduleSendRaport(contact);
+                            // an email/fax/hl7 should be scheduled to be sent to the contact between the hours he set
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void sendRaport(Contact contact)
+        {
+            Console.WriteLine(contact.delivery + " sent to " + contact.name);
+        }
+
+        static void scheduleSendRaport(Contact contact)
+        {
+            Console.WriteLine(contact.delivery + " will be sent to " + contact.name + ' ' + contact.deliveryHours);
+        }
+    }
+}

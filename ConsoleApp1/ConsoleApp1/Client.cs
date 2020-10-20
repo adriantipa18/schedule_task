780b5f93795316cfbb80996816ec9e0f858dec6e
Adrian <Adrian@DESKTOP-JE8H084>
2020-10-20 12:51:24 +0300

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Client
+    {
+        public string name { get; set; }
+        public List<Contact> contacts { get; set; }
+
+        public Client(string name, List<Contact> contacts)
+        {
+            this.name = name;
+            this.contacts = contacts;
+        }
+
+        public Client(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Contact
+    {
+        public String name { get; set; }
+        public String delivery { get; set; }
+        public String deliveryHours { get; set; }
+
+        public Contact(string name, string delivery, string deliveryHours)
+        {
+            this.name = name;
+            this.delivery = delivery;
+            this.deliveryHours = deliveryHours;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
